ab1f83a52673a7145b0f6bc6da5a1e6552659319
Xavier Lavallee <xavier.lavallee@gmail.com>
2024-07-13 21:55:47 -0400

    Add symlinks for existing dotfiles
    Moved all of my dotfiles into the repo and symlinked them.

---
+++ symlink.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -f -s ~/.dotfiles/nvim/ ~/.config
+ln -f -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -f -s ~/.dotfiles/tmux/.tmux.conf ~
+ln -f -s ~/.dotfiles/ngrok/.ngrok2/ ~
+ln -f -s ~/.dotfiles/wezterm/.wezterm.lua ~
+ln -f -s ~/.dotfiles/git/.gitconfig ~
