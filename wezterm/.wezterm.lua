5c99e6dad28e5bfe6e27cc1730f0dc4606efae12
Xavier Lavallee <xavier.lavallee@gmail.com>
2024-07-15 09:46:37 -0400

    Sync wezterm and tmux theme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Tokyo Night Moon"
+config.color_scheme = "Tokyo Night"
 config.font = wezterm.font("Fira code")
 config.font_size = 15
 config.hide_tab_bar_if_only_one_tab = true
