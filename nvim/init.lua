f21cc901f90a2cca063b1e091e6e1bcb5151b44a
Xavier Lavallee <xavier.lavallee@gmail.com>
2024-07-25 21:39:40 -0400

    Add barbar tab manager to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -638,6 +638,7 @@ require('lazy').setup({
   require 'plugins.gitsigns',
   require 'plugins.git-blame',
   require 'plugins.noice',
+  require 'plugins.barbar',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
