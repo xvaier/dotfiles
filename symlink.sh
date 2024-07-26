c13e5f942066b87904d52ea003aeba825afed7f2
Xavier Lavallee <xavier.lavallee@gmail.com>
2024-07-25 21:05:27 -0400

    Add missing symlinks

--- symlink.sh
+++ symlink.sh
@@ -4,6 +4,7 @@ ln -f -s ~/.dotfiles/nvim/ ~/.config
 ln -f -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -f -s ~/.dotfiles/zsh/.zimrc ~/.zimrc
 ln -f -s ~/.dotfiles/tmux/.tmux.conf ~
+ln -f -s ~/.dotfiles/tmux/tmux-spotify ~/.tmux/tmux-spotify
 ln -f -s ~/.dotfiles/ngrok/.ngrok2/ ~
 ln -f -s ~/.dotfiles/wezterm/.wezterm.lua ~
 ln -f -s ~/.dotfiles/git/.gitconfig ~
