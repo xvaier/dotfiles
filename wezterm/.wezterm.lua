c93e3f52de4c8211c50ee2f4fd30c193a9f8a828
Xavier Lavallee <xavier.lavallee@gmail.com>
2024-08-07 17:32:38 -0400

    Increase wezterm font size

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Tokyo Night"
 config.font = wezterm.font("Fira code")
-config.font_size = 15
+config.font_size = 16
 config.hide_tab_bar_if_only_one_tab = true
 
 return config
