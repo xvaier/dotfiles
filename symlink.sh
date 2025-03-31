ff43e1ffb890bfd5ae41a2f291dcf3579ae767ed
Xavier Lavallee <xavier.lavallee@gmail.com>
2025-03-31 16:45:17 -0400

    Add theme for bat

---
+++ symlink.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p ~/.config/
+ln -sfn ~/.dotfiles/nvim ~/.config/nvim
+ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/zsh/.zimrc ~/.zimrc
+ln -sf ~/.dotfiles/zsh/.fzf.zsh ~/.fzf.zsh
+ln -sf ~/.dotfiles/tmux/.tmux.conf ~
+ln -sf ~/.dotfiles/tmux/tmux-spotify ~/.tmux/tmux-spotify
+ln -sf ~/.dotfiles/wezterm/.wezterm.lua ~
+ln -sfn ~/.dotfiles/bat ~/.config/bat 
