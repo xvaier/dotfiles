e91ea2fd4edd51f13acfc1b6680763c2a8a90cf2
Xavier Lavallee <xavier.lavallee@gmail.com>
2025-04-25 22:43:32 -0400

    Update fuzzy finder commands

--- nvim/lua/plugins/fuzzfind.lua
+++ nvim/lua/plugins/fuzzfind.lua
@@ -2,16 +2,22 @@ return {
   {
     "ibhagwan/fzf-lua",
     keys = {
-      { "<Leader>sf", function() require("fzf-lua").files() end,      desc = "Search files" },
-      { "<Leader>sg", function() require("fzf-lua").git_files() end,  desc = "Search Git files" },
-      { "<Leader>sc", function() require("fzf-lua").git_status() end, desc = "Search Changed files" },
-      { "<Leader>sb", function() require("fzf-lua").buffers() end,    desc = "Search Buffers" },
-      { "<Leader>sr", function() require("fzf-lua").live_grep() end,  desc = "Search Ripgrep" },
-      { "<Leader>sl", function() require("fzf-lua").lines() end,      desc = "Search lines in open buffers" },
+      { "<Leader>sf", function() require("fzf-lua").files() end,                 desc = "Search files" },
+      { "<Leader>sr", function() require("fzf-lua").resume() end,             desc = "Search Git files" },
+      { "<Leader>sc", function() require("fzf-lua").git_status() end,            desc = "Search Changed files" },
+      { "<Leader>sb", function() require("fzf-lua").buffers() end,               desc = "Search Buffers" },
+      { "<Leader>sg", function() require("fzf-lua").live_grep() end,             desc = "Search Ripgrep" },
+      { "<Leader>sl", function() require("fzf-lua").lines() end,                 desc = "Search lines in open buffers" },
+      { "<Leader>sd", function() require("fzf-lua").diagnostics_workspace() end, desc = "Search workspace diagnostics" },
     },
     opts = {
-      {
-        winopts = { preview = { default = "bat" } }
+      winopts = {
+        backdrop = 100,
+        height = 0.95,
+        width = 0.95,
+        preview = {
+          layout = "vertical"
+        }
       }
     }
   }
