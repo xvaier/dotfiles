618df951f64e14fee5f01f6130d916d810595cac
Xavier Lavallee <xavier.lavallee@gmail.com>
2025-04-30 14:50:07 -0400

    Add mason for most lsps

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,5 +1,21 @@
 return {
-  'williamboman/mason.nvim',
-  lazy = false,
-  opts = {},
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "eslint",
+          "lua_ls",
+          "basedpyright",
+          "ruff",
+          "ts_ls",
+        },
+        automatic_installation = true,
+      })
+    end
+  }
 }
