b044a9fcdca11f84beb09a334965603773ed6219
Xavier Lavallee <xavier.lavallee@gmail.com>
2025-04-29 22:04:52 -0400

    Improve git config

--- symlink.sh
+++ symlink.sh
@@ -7,7 +7,7 @@ ln -sf ~/.dotfiles/zsh/.fzf.zsh ~/.fzf.zsh
 ln -sf ~/.dotfiles/tmux/.tmux.conf ~
 ln -sf ~/.dotfiles/tmux/tmux-spotify ~/.tmux/tmux-spotify
 ln -sfn ~/.dotfiles/ghostty ~/.config/ghostty
- 
+ln -sf ~/.dotfiles/git/.gitconfig ~/.gitconfig 
 # Setup bat config
 ln -sfn ~/.dotfiles/bat ~/.config/bat 
 bat cache --build
