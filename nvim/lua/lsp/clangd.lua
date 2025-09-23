b35e9e158f4a79a54ad5ab23db2fe8ad10aa29d8
Xavier Lavallee <xavier.lavallee@gmail.com>
2025-09-23 14:07:56 -0400

    Add c support and fix ghostty config

---
+++ nvim/lua/lsp/clangd.lua
@@ -0,0 +1,30 @@
+vim.lsp.config['clangd'] = {
+  cmd = { "clangd" },
+  capabilities = {
+    offsetEncoding = { "utf-8", "utf-16" },
+    textDocument = {
+      completion = {
+        editsNearCursor = true
+      }
+    }
+  },
+  filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+  root_markers = { ".clangd", ".clang-tidy", ".clang-format", "compile_commands.json", "compile_flags.txt", "configure.ac", ".git" },
+  on_init = function(client, init_result)
+    if init_result.offsetEncoding then
+      client.offset_encoding = init_result.offsetEncoding
+    end
+  end,
+  on_attach = function(client, bufnr)
+    vim.api.nvim_buf_create_user_command(bufnr, 'LspClangdSwitchSourceHeader', function()
+      switch_source_header(bufnr, client)
+    end, { desc = 'Switch between source/header' })
+
+    vim.api.nvim_buf_create_user_command(bufnr, 'LspClangdShowSymbolInfo', function()
+      symbol_info(bufnr, client)
+    end, { desc = 'Show symbol info' })
+  end,
+  settings = {}
+}
+
+vim.lsp.enable('clangd')
