4b0edf669d93a0038eebea9620137bcafd435adc
Xavier Lavallee <xavier.lavallee@gmail.com>
2026-08-02 22:07:45 -0400

    Add git support to oil

--- nvim/lua/plugins/filemanager.lua
+++ nvim/lua/plugins/filemanager.lua
@@ -1,29 +1,32 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {
-    keymaps = {
-      ["g?"] = { "actions.show_help", mode = "n" },
-      ["<C-s>"] = false,
-      ["<C-h>"] = false,
-      ["<C-t>"] = false,
-      ["<C-p>"] = "actions.preview",
-      ["<C-c>"] = { "actions.close", mode = "n" },
-      ["<C-l>"] = false,
-      ["-"] = { "actions.parent", mode = "n" },
-      ["_"] = { "actions.open_cwd", mode = "n" },
-      ["`"] = { "actions.cd", mode = "n" },
-      ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-      ["gs"] = { "actions.change_sort", mode = "n" },
-      ["gx"] = "actions.open_external",
-      ["g."] = { "actions.toggle_hidden", mode = "n" },
-      ["g\\"] = { "actions.toggle_trash", mode = "n" },
-    }
-  },
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  lazy = false,
-  keys = {
-    { "-", "<Cmd>Oil<CR>", desc = "Open file manager" },
-  },
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {
+			keymaps = {
+				["g?"] = { "actions.show_help", mode = "n" },
+				["<C-s>"] = false,
+				["<C-h>"] = false,
+				["<C-t>"] = false,
+				["<C-p>"] = "actions.preview",
+				["<C-c>"] = { "actions.close", mode = "n" },
+				["<C-l>"] = false,
+				["-"] = { "actions.parent", mode = "n" },
+				["_"] = { "actions.open_cwd", mode = "n" },
+				["`"] = { "actions.cd", mode = "n" },
+				["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+				["gs"] = { "actions.change_sort", mode = "n" },
+				["gx"] = "actions.open_external",
+				["g."] = { "actions.toggle_hidden", mode = "n" },
+				["g\\"] = { "actions.toggle_trash", mode = "n" },
+			},
+		},
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		lazy = false,
+		keys = {
+			{ "-", "<Cmd>Oil<CR>", desc = "Open file manager" },
+		},
+	},
+	{ "malewicz1337/oil-git.nvim", dependencies = { "stevearc/oil.nvim" } },
 }
