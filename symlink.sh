efae379a8f2749711e3bd2e830279f9307f3e2c2
Xavier Lavallee <xavier.lavallee@gmail.com>
2026-08-13 21:34:29 -0400

    Track zprofile, zshenv, ssh config and the global gitignore

--- symlink.sh
+++ symlink.sh
@@ -4,6 +4,8 @@ ln -sfn ~/.dotfiles/nvim ~/.config/nvim
 
 # zsh
 ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/zsh/zprofile ~/.zprofile
+ln -sf ~/.dotfiles/zsh/zshenv ~/.zshenv
 ln -sf ~/.dotfiles/zsh/zimrc ~/.zimrc
 ln -sf ~/.dotfiles/zsh/fzf.zsh ~/.fzf.zsh
 ln -sf ~/.dotfiles/zsh/flare.zsh ~/.flare.zsh
@@ -12,6 +14,10 @@ ln -sf ~/.dotfiles/zsh/git-worktree.zsh ~/.git-worktree.zsh
 ln -sf ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -sfn ~/.dotfiles/ghostty ~/.config/ghostty
 ln -sf ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/git/global.gitignore ~/.global.gitignore
+
+mkdir -p ~/.ssh
+ln -sf ~/.dotfiles/ssh/config ~/.ssh/config
 
 # Symlink the 1password ssh sock agent
 mkdir -p ~/.1password && ln -s ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
