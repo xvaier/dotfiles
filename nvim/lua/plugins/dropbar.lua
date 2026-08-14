f2f9eedda13dd83fcddc95c99895d644d445bff3
Xavier Lavallee <xavier.lavallee@gmail.com>
2026-08-14 12:50:51 -0400

    Remove disabled plugin specs and unreferenced helpers
    avante.lua and dropbar.lua were both enabled = false, and neither was in
    the lockfile. helpers/nodejs.lua and helpers/python.lua lost their last
    callers in b7b1e27.

--- nvim/lua/plugins/dropbar.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'Bekaboo/dropbar.nvim',
-  enabled = false,
-  opts = { icons = { enable = false } },
-  config = function()
-    local dropbar_api = require('dropbar.api')
-    vim.keymap.set('n', '<Leader>wb', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
-  end
-}
